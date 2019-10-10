72fa585957647b9d7b8a32ebe65bbe4286c38cd4
apisitprompha <apisitprompha@gmail.com>
2019-10-10 16:55:51 +0700

    ADD API

--- src/services/API.js
+++ src/services/API.js
@@ -13,3 +13,26 @@ export const sellerRole = async (data, token = "") => {
     const res = await Fetch("POST", token, `/seller/role`, data);
     return res;
 };
+//------------------ SHIPPING -------------------//
+export const shippigGet = async (data, token = "") => {
+    const res = await Fetch("GET", token, `/shipping/lists`, data);
+    return res;
+};
+//------------------ PAYMENT -------------------//
+export const paymentGet = async (data, token = "") => {
+    const res = await Fetch("GET", token, `/payment/all`, data);
+    return res;
+};
+export const paymentCheck = async (data, token = "") => {
+    const res = await Fetch("GET", token, `/admin/check`, data);
+    return res;
+};
+export const paymentPay = async (data, token = "") => {
+    const res = await Fetch("POST", token, `/payment/pay`, data);
+    return res;
+};
+//------------------ RECIEVE -------------------//
+export const paymentRecieve = async (data, token = "") => {
+    const res = await Fetch("GET", token, `/shipping/recieve`, data);
+    return res;
+};
