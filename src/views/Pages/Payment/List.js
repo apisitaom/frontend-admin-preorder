8bbb0ced1ad1a2d28613749599a5bc9cd50e5af2
apisitprompha <apisitprompha@gmail.com>
2019-10-22 15:07:41 +0700

    เพิ่มปุ่ม search

--- src/views/Pages/Payment/List.js
+++ src/views/Pages/Payment/List.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Input } from 'antd'
 import PaymentFrom from '../../../from/paymentfrom/PaymentFrom'
 import PaidFrom from '../../../from/paymentfrom/PaidFrom'
 import { paymentPaid, paymentCheck, paymentRole } from '../../../services/API'
@@ -7,7 +7,9 @@ export default class List extends Component {
     state = {
         paids: [],
         payments: [],
-        results: []
+        results: [],
+        searchnamepayment: '',
+        search: false,
     }
     UNSAFE_componentWillMount () {
         this.getPaid();
@@ -42,13 +44,37 @@ export default class List extends Component {
         await paymentRole(data);
         await window.location.reload();
     }
+    searchPayment = e => {
+        this.setState({searchnamepayment: e.target.value},()=> {
+            let dataSearch
+            if(this.state.searchnamepayment !== ''){
+                dataSearch = this.state.payments.filter(value=> {
+                    return (value.fullname.toLowerCase().indexOf(this.state.searchnamepayment.toLowerCase()) > -1)
+                })
+                this.setState({dataSearch: [...dataSearch],search: true})
+            }else{
+                this.setState({search: false})
+            }
+        } 
+        )
+    }
     render() {
         return (
             <div>
-                <Row gutter={4}>   
+                <Row>
+                    <Col offset={4} span={8} >
+                        <Input 
+                        style={{width: '100%', textAlign: 'center', height: '35px' }}
+                        placeholder="ค้นหา/ชื่อลูกค้า ?"
+                        value={this.state.searchnamepayment}
+                        onChange={(e)=>this.searchPayment(e)}
+                        />
+                    </Col>
+                </Row>
+                <Row gutter={4} style={{marginTop: '5px'}}>   
                     <Col span={12}>
                         <PaymentFrom 
-                        data={this.state.payments.length > 0 ? this.state.payments :[]}
+                        data={this.state.search ? this.state.dataSearch : this.state.payments.length > 0 ? this.state.payments :[]}
                         showModal={this.showModalPayment}
                         payment={this.state.payment}
                         changeStatus={this.onUpdatetatusPayment}
--- src/views/Pages/Shipping/List.js
+++ src/views/Pages/Shipping/List.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Input } from 'antd'
 import ShippingFrom from '../../../from/shippingfrom/ShippingFrom'
 import { shippigGet } from '../../../services/API'
 export default class List extends Component {
     state = {
         shippings: [],
+        searchname: '',
+        search: false
     }
     UNSAFE_componentWillMount () {
         this.getShipping();
@@ -21,14 +23,38 @@ export default class List extends Component {
             visible: true
         })
     }
+    search = e => {
+        this.setState({searchname: e.target.value},()=> {
+            let dataSearch
+            if(this.state.searchname !== ''){
+                dataSearch = this.state.shippings.filter(value=> {
+                    return (value.fullname.toLowerCase().indexOf(this.state.searchname.toLowerCase()) > -1)
+                })
+                this.setState({dataSearch: [...dataSearch],search: true})
+            }else{
+                this.setState({search: false})
+            }
+        } 
+        )
+    }
     render() {
         return (
             <div>
                 <Row>
+                    <Col offset={16}>
+                        <Input 
+                        style={{width: '100%', textAlign: 'center', height: '35px' }}
+                        placeholder="ค้นหา/ชื่อร้านค้า ?"
+                        value={this.state.searchname}
+                        onChange={(e)=>this.search(e)}
+                        />
+                    </Col>
+                </Row>
+                <Row style={{marginTop: '5px'}}>
                     <Col>
                     <ShippingFrom 
                     status={this.setState.status = "shipping"}
-                    data={this.state.shippings.length > 0 && this.state.shippings}
+                    data={this.state.search ? this.state.dataSearch : this.state.shippings.length > 0 && this.state.shippings}
                     showModal={this.showModal}
                     shipping={this.state.shipping}
                     />
