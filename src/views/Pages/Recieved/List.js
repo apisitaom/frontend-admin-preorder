52700557bbf18bd0f5cd78c706bb612df14b6d3e
apisitprompha <apisitprompha@gmail.com>
2019-10-22 17:43:23 +0700

    ลบปุ่ม search recieve

--- src/views/Pages/Recieved/List.js
+++ src/views/Pages/Recieved/List.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import { Row, Col, Input } from 'antd'
+import { Row, Col } from 'antd'
 import Shippingfrom from '../../../from/shippingfrom/ShippingFrom'
 import { shippingRecieve } from '../../../services/API'
 export default class List extends Component {
     state = {
         shippings : [],
-        searchname: '',
-        search: false
     }
     UNSAFE_componentWillMount () {
         this.getRecieve();
@@ -23,33 +21,9 @@ export default class List extends Component {
             visible: true
         })
     }
-    search = e => {
-        this.setState({searchname: e.target.value},()=> {
-            let dataSearch
-            if(this.state.searchname !== ''){
-                dataSearch = this.state.shippings.filter(value=> {
-                    return (value.fullname.toLowerCase().indexOf(this.state.searchname.toLowerCase()) > -1)
-                })
-                this.setState({dataSearch: [...dataSearch],search: true})
-            }else{
-                this.setState({search: false})
-            }
-        } 
-        )
-    }
     render() {
         return (
             <div>
-                <Row>
-                    <Col offset={16}>
-                        <Input 
-                        style={{width: '100%', textAlign: 'center', height: '35px' }}
-                        placeholder="ค้นหา/ชื่อลูกค้า ?"
-                        value={this.state.searchname}
-                        onChange={(e)=>this.search(e)}
-                        />
-                    </Col>
-                </Row>
                 <Row style={{marginTop: '5px'}}>
                     <Col>
                     <Shippingfrom 
